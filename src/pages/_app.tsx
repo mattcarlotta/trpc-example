122b9c0096bf564d4434d70785d9b7b5a4a3a475
Matt <carlotta.matt@gmail.com>
2022-09-10 18:50:33 -0700

    chore: add cache control

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,18 @@
 import { createTRPCClient } from '@trpc/client'
+import { httpLink } from '@trpc/client/links/httpLink'
 import { AppProps } from 'next/app'
 import '../styles/globals.css'
 import getBaseUrl from '../utils/getBaseUrl'
 import { trpc } from '../utils/trpc'
 
-const client = createTRPCClient({ url: getBaseUrl() + '/api/trpc' })
+const client = createTRPCClient({
+  url: getBaseUrl() + '/api/trpc',
+  links: [
+    httpLink({
+      url: '/api/trpc'
+    })
+  ]
+})
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
--- src/server/router.ts
+++ src/server/router.ts
@@ -1,9 +1,26 @@
 import * as trpc from '@trpc/server'
+import { inferAsyncReturnType } from '@trpc/server'
+import * as trpcNext from '@trpc/server/adapters/next'
 import { z } from 'zod'
 import { Posts, Users } from './types'
 
-export const appRouter = trpc
-  .router()
+export const createContext = async ({
+  req,
+  res
+}: trpcNext.CreateNextContextOptions) => {
+  return {
+    req,
+    res
+  }
+}
+
+type Context = inferAsyncReturnType<typeof createContext>
+
+function createRouter() {
+  return trpc.router<Context>()
+}
+
+export const appRouter = createRouter()
   .query('users', {
     input: z.void(),
     output: Users,
