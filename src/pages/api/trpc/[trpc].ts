3f93439119852cae5d643b8da8fe147dd84a941b
Matt <carlotta.matt@gmail.com>
2022-09-15 19:10:18 -0700

    chore: remove cache

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -4,17 +4,5 @@ import { appRouter } from '../../../server/router'
 // export API handler
 export default trpcNext.createNextApiHandler({
   router: appRouter,
-  createContext: () => null,
-  responseMeta({ type, errors }) {
-    return errors.length === 0 && type === 'query'
-      ? {
-          headers: {
-            'cache-control': `s-maxage=1, stale-while-revalidate=${
-              60 * 60 * 24
-            }`,
-            'cache-date': `${new Date().toUTCString()}`
-          }
-        }
-      : {}
-  }
+  createContext: () => null
 })
